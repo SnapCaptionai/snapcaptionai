dab97f7f6ffb862b78690928cb86f82649e7a97e
Shatica R Miles <whosttv@gmail.com>
2026-04-16 15:32:00 -0400

    Increase maxOutputTokens to 8000 in generate.js

--- api/generate.js
+++ api/generate.js
@@ -38,7 +38,7 @@ export default async function handler(req, res) {
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({
             contents: [{ role: 'user', parts: [{ text: prompt }] }],
-            generationConfig: { maxOutputTokens: 2000 }
+            generationConfig: { maxOutputTokens: 8000 }
           })
         }
       );
